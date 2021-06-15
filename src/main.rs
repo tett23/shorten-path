8a2b38432b4a73ab2859718cb4147e493dcbe9c5
tett23 <tett23@gmail.com>
2021-06-15 19:23:46 +0900

    Fix path logic

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{env, path::Path, path::PathBuf, path::MAIN_SEPARATOR};
 fn main() {
     let arg = env::args();
     let path = arg.last();
-    let path = path.expect("");
+    let path = path.expect("msg");
 
     let rules: Vec<Box<dyn Fn(String) -> String>> = vec![box replace_home, box shorten_dirs];
     let ret = rules.iter().fold(path, |acc, f| f(acc));
@@ -19,8 +19,8 @@ fn main() {
 
 fn replace_home(buf: String) -> String {
     let home: Option<PathBuf> = dirs::home_dir();
-    let home = home.expect("");
-    let home = home.to_str().unwrap().to_owned();
+    let home = home.expect("msg");
+    let home = home.to_str().expect("msg").to_owned();
 
     match buf.starts_with(&home) {
         true => buf.replace(&home, "~"),
@@ -32,27 +32,31 @@ fn shorten_dirs(buf: String) -> String {
     match buf.starts_with("~") {
         true => {
             let path = Path::new(&buf);
-            let file_name = path
+            let file_name: &str = &path
                 .file_name()
                 .expect("msg")
                 .to_str()
                 .expect("msg")
                 .to_string();
 
-            let parent_elems = path
+            let parent_elems: &str = &path
                 .parent()
                 .expect("msg")
                 .to_str()
                 .expect("msg")
                 .split(MAIN_SEPARATOR)
-                .map(|v| v.chars().next().expect(""))
+                .filter(|v| !v.is_empty())
+                .map(|v| v.chars().next().expect("msg"))
                 .map(|v| v.to_string())
                 .collect::<Vec<String>>()
                 .join(&MAIN_SEPARATOR.to_string());
 
-            let tmp: Vec<&str> = vec![&parent_elems, &file_name];
+            let tmp: Vec<&str> = vec![parent_elems, file_name];
 
-            tmp.join(&MAIN_SEPARATOR.to_string())
+            tmp.into_iter()
+                .filter(|v| !v.is_empty())
+                .collect::<Vec<&str>>()
+                .join(&MAIN_SEPARATOR.to_string())
         }
         false => buf,
     }
